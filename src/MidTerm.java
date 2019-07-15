325579bf9761a265ab72441ffa5833159fbadeb2
CloudWork <baraasaeed@yahoo.com>
2019-07-15 10:05:39 -0400

    initial commit

---
+++ src/MidTerm.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2019. This program and the accompanying materials are made
+ * available under my granted permission provided that this note is kept intact,
+ * unmodified and unchanged. @ Author: Baraa Ali - API and implementation. All
+ * rights reserved.
+ */
+
+public class MidTerm {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"Once upon a time in the mystical land of Shangrila \n there dwelt an immortal named Kai-Yun and his trusty \n band of ninja monkeys.");
+
+	}
+
+}
