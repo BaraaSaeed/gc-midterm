542d3fd2ab6caa607947ff716288601a861ebe2d
sienna <siennah13@gmail.com>
2019-07-15 11:20:02 -0400

    students

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -9,11 +9,11 @@ public class MidTerm {
 
 	public static void main(String[] args) {
 		System.out.println(
-				"Once upon a time in the mystical land of Detroit \n there dwelt an immortal named Kai-Yun and his trusty \n band of ninja monkeys. They were very clever monkeys indeed, known for their ingenious teamwork.");
+				"Once upon a time in the mystical land of Detroit \n there dwelt an immortal named Kai-Yun and his trusty \n band of ninja students. They were very clever students indeed, known for their ingenious teamwork.");
 		// Sienna
 
 		System.out.println(
-				"But following Kai-Yun’s unifying leadership, they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the monkeys.");
+				"But following Kai-Yun’s unifying leadership, they paired up and mowed down their foes. One-by-one every last pesky bug fell at the quick hands of the students.");
 		// Baraa
 		System.out.println("Hi team! It is me Baraa Ali");
 
